dd301602ae19d38ca27b7c6cb263f6e659c3d146
sorec <sorec@HP-Sorecar.mshome.net>
2020-03-23 21:36:56 -0600

    Creacion del repositorio

---
+++ autobus/src/test/java/com/servicios/autobus/AutobusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicios.autobus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
